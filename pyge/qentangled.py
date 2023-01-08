1e450da3865cdac3a08ad3e90e1629ad6a7435c6
loscati <leonardo.salicari.physics@gmail.com>
2023-01-08 12:33:39 +0100

    qentanglement function init impl

--- pyge/qentangled.py
+++ pyge/qentangled.py
@@ -1,18 +1,71 @@
 """
 Functions to compute the fraction of entangled contacts
 
+The module implement the function to compute the fraction of
+entangled contacts and this value from a timeseries.
+Differently from trajectory and gent, here the functions takes
+as input the list produced by gent.ge_loops
+
 The function takes as argument the list of contacts together with info
 about the respective thread and G' value. This list is the one
 produced by gent.ge_loops
 """
 import numpy as np
 
-# funzione che prende la lista gia calcolata
-# gradi di liberta': funzione di attivazine, G0, w
+def hill_fun(inp, threshold, hill_coeff) -> np.ndarary:
+    """
+    Hill function
 
-def hill_fun(inp, threshold, hill_coeff):
+    Parameters
+    ----------
+    inp : array_like
+        float or array for the x value
+    threshold : float
+        value characterizing the cutoff for the function
+    hill_coeff : float or int
+        exponent of the function
+
+    Returns
+    -------
+    np.ndarray
+        hill function for the parameters provided
+    """
     return 1/( 1 + (threshold/inp)**hill_coeff )
 
-def qentangled(contact_thr_ge_list, activation_fun, activation_params):
-    pass
-    
+def qentangled(contact_thr_ge_list, n_contacts, min_loop_len=0, activation_fun=hill_fun, activation_params=None) -> float:
+    r"""
+    Compute the fraction of entangled contacts defined as:
+        \frac{1}{N} \sum_{formed contacts} \sgn (G') f( |G'| )
+
+    Parameters
+    ----------
+    contact_thr_ge_list : see gent.ge_loops
+        List of loops, relative thread and G' value
+    n_contacts : int
+        total number of contacts
+    min_loop_len : int, optional
+        Minimal number of residues to identify a chain segment as a loop, by default 0
+    activation_fun : function, optional
+        Python function used as activation one, by default hill_fun
+    activation_params : dict, optional
+        Parameters passed to the activation function;
+        The input of the function must be the first argument, while
+        those passed through this dict are subsequent
+        By default None => {"thr":0.7, "hill_coeff":4}
+
+    Returns
+    -------
+    float
+        Fraction of entangled contacts
+    """
+    if activation_params is None:
+        activation_params = {"thr":0.7, "hill_coeff":4}
+
+    # extract ge filtering wrt loop len. Default is no filtering
+    ge_array = np.array([x for x in contact_thr_ge_list if x[0][1]-x[0][0] >= min_loop_len])
+
+    # np.sign -> 0 if the argument is 0. Ok because otherwise the
+    # activation function would be 0 (and it is test to be in this way)
+    # Moreover, the activation function takes as input the abs of G'
+    integrand = np.sign(ge_array)*activation_fun(np.abs(ge_array), **activation_params)
+    return integrand.sum()/n_contacts
--- tests/qentangled_test.py
+++ tests/qentangled_test.py
@@ -1,19 +1,40 @@
 """
 Test functions for the qentangled module
 """
+import pathlib
+
 import pytest
 import numpy as np
 
-from pyge.qentangled import qentangled
+from pyge.qentangled import hill_fun
+
+@pytest.mark.parametrize("imp, thr, hill_coeff", [
+    (0.001, 0.5, 2),
+    (0.001, 0.5, 4),
+    (0.001, 0.7, 2),
+    (0.001, 0.7, 4)
+])
+def test_hill_fun_left_extrema(imp, thr, hill_coeff):
+    """Test extrema: result should tend to 0"""
+    assert hill_fun(imp, thr, hill_coeff) < 1e-4
+
+@pytest.mark.parametrize("imp, thr, hill_coeff", [
+    (1.5, 0.5, 4),
+    (2, 0.7, 4)
+])
+def test_hill_fun_right_extrema(imp, thr, hill_coeff):
+    """Test extrema: result should tend to 1"""
+    assert hill_fun(imp, thr, hill_coeff) > 0.95
+
+def test_qentangled():
+    """Results should be consistent with independent calculations"""
+    parent = pathlib.Path(__file__).parent.resolve()
+
+    ge_1ucs_native = np.load(
+        parent / "data/GE_1ucs_native.npy",
+        allow_pickle=True)
+
 
 # 1 riprodurre condizione dei file di antonio, ovvero modificare la lista di contatti
 # per 1ucs e qualche altra proteina
 # 2. calcolo con la mia funzinoe e asser
-
-@pytest.mark.parametrize("input, thr, hill_coeff", [
-    (0.0001, 0.5, 1),
-    (0.0001, 0.5, 4)
-])
-def hill_fun_left_extrema(imp, thr, hill_coeff):
-    # test extrema
-    assert np.isclose(qentangled.hill_fun(imp, thr, hill_coeff), 0)
