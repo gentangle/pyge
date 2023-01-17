b801599dc74257f868855162e9127fc1ce062c80
loscati <leonardo.salicari.physics@gmail.com>
2023-01-17 11:35:30 +0100

    resolve merge conflict w main

--- pyge/activation.py
+++ pyge/activation.py
@@ -5,7 +5,7 @@ import numpy as np
 from numba import njit
 
 @njit
-def hill_fun(inp: np.ndarray, threshold: float, hill_coeff: float) -> np.ndarray:
+def hill_fun(inp:np.ndarray, threshold:float, hill_coeff:float) -> np.ndarray:
     """
     Hill function
 
@@ -17,10 +17,17 @@ def hill_fun(inp: np.ndarray, threshold: float, hill_coeff: float) -> np.ndarray
         value characterizing the cutoff for the function
     hill_coeff : float or int
         exponent of the function
-
     Returns
     -------
     np.ndarray
         hill function for the parameters provided
     """
-    return 1/( 1 + (threshold/inp)**hill_coeff )
+    # use numpy array even if a single float is provided for a correct
+    # handle of 0 (because we divide for 0 even thought the function tends to 0
+    # for inp going to 0)
+    inp = np.asarray(inp)
+    out = 1/( 1 + (threshold/inp)**hill_coeff )
+    # check fo NaN
+    if np.isnan(np.sum(out)):
+        raise RuntimeError("NaNs are detected as output of the Hill function")
+    return out
