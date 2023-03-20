35ec2c05c3620df03cac8deed49a96dc39e0200c
loscati <leonardo.salicari.physics@gmail.com>
2023-03-20 23:15:46 +0100

    use of GE and GETermini

--- tests/singlechain_test.py
+++ tests/singlechain_test.py
@@ -40,13 +40,19 @@ def test_ge_from_pdb():
         },
         selection_options=None,
     )
-    for res, exp in zip(result_1.loop_thr_ge, ge_1ucs_native):
-        assert res[0] == exp[0]
-        assert res[1] == exp[1]
-        assert np.isclose(res[2], exp[2])
-    assert result_1.ge_max[0] == expected_output_1["ge_max"][0]
-    assert result_1.ge_max[1] == expected_output_1["ge_max"][1]
-    assert np.isclose(result_1.ge_max[2], expected_output_1["ge_max"][2], atol=0.0001)
+    for res, exp in zip(result_1.loops, ge_1ucs_native):
+        if abs(res.n_term.value) >= abs(res.c_term.value):
+            ge2com = res.n_term
+        else:
+            ge2com = res.c_term
+        assert ge2com.loop == exp[0]
+        assert ge2com.thread == exp[1]
+        assert np.isclose(ge2com.value, exp[2])
+    assert result_1.global_max.loop == expected_output_1["ge_max"][0]
+    assert result_1.global_max.thread == expected_output_1["ge_max"][1]
     assert np.isclose(
-        result_1.ge_weighted[2], expected_output_1["ge_weighted"][2], atol=0.0001
+        result_1.global_max.value, expected_output_1["ge_max"][2], atol=0.0001
+    )
+    assert np.isclose(
+        result_1.global_weighted.value, expected_output_1["ge_weighted"][2], atol=0.0001
     )
